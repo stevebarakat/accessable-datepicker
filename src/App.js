9973f8e8cdc392a882e9d4c303fe8be59d10ec07
Steve <stevebarakat@gmail.com>
2021-05-24 13:12:57 -0400

    checking out

--- src/App.js
+++ src/App.js
@@ -1,65 +1,68 @@
-import React, { useEffect, useRef, useReducer } from "react";
+import React, { useState, useEffect, useRef, useReducer } from "react";
 import { format, getDate, isEqual } from "date-fns";
-import { useGenerateDays, useGenerateDates } from "./hooks";
+import { useGenerateDays, useGenerateDates, usePrevious } from "./hooks";
 import selectedDateReducer from "./selectedDateReducer";
-import {handleTableKeyPress} from './handleTableKeyPress';
 import Header from "./Header";
 import "./App.css";
+
+function useHandleTableKeyPress(e, state, dispatch) {
+  // Check if shift key was pressed
+  const shift = e?.shiftKey;
+  switch (e?.keyCode) {
+    case 13: //Enter
+    case 32: //Space
+      dispatch({ type: "SET_DATE", payload: state.focusedDate });
+      return;
+    case 27: //Esc
+      // close calendar
+      return;
+    case 33: //Page Up
+      shift
+        ? dispatch({ type: "SET_DATE_NEXT_YEAR" })
+        : dispatch({ type: "SET_DATE_NEXT_MONTH" });
+      return;
+    case 34: //Page Down
+      shift
+        ? dispatch({ type: "SET_DATE_PREVIOUS_YEAR" })
+        : dispatch({ type: "SET_DATE_PREVIOUS_MONTH" });
+      return;
+    case 35: //End
+      dispatch({ type: "SET_MONTH_END" });
+      return;
+    case 36: //Home
+      dispatch({ type: "SET_MONTH_START" });
+      return;
+    case 37: //Left
+      dispatch({ type: "SET_PREVIOUS_DAY" });
+      return;
+    case 38: //Up
+      dispatch({ type: "SET_PREVIOUS_WEEK" });
+      return;
+    case 39: //Right
+      dispatch({ type: "SET_NEXT_DAY" });
+      return;
+    case 40: //Down
+      dispatch({ type: "SET_NEXT_WEEK" });
+      return;
+    default:
+      return;
+  }
+}
 
 const initialDate = new Date();
 const initialState = { selectedDate: initialDate, focusedDate: initialDate };
 const App = () => {
   const [state, dispatch] = useReducer(selectedDateReducer, initialState);
+  const prevState = usePrevious(state);
+  const [e, setE] = useState(null);
   const focusedDateRef = useRef(null);
   const dates = useGenerateDates(state.selectedDate);
   const days = useGenerateDays({ length: 2 });
-
   useEffect(() => focusedDateRef.current?.focus());
+  useHandleTableKeyPress(() => e, state, dispatch);
 
-  // function handleTableKeyPress(e) {
-  //   // Check if shift key was pressed
-  //   const shift = e.shiftKey;
-  //   switch (e.keyCode) {
-  //     case 13: //Enter
-  //     case 32: //Space
-  //       dispatch({ type: "SET_DATE", payload: state.focusedDate });
-  //       return;
-  //     case 27: //Esc
-  //       // close calendar
-  //       return;
-  //     case 33: //Page Up
-  //       shift
-  //         ? dispatch({ type: "SET_DATE_NEXT_YEAR" })
-  //         : dispatch({ type: "SET_DATE_NEXT_MONTH" });
-  //       return;
-  //     case 34: //Page Down
-  //       shift
-  //         ? dispatch({ type: "SET_DATE_PREVIOUS_YEAR" })
-  //         : dispatch({ type: "SET_DATE_PREVIOUS_MONTH" });
-  //       return;
-  //     case 35: //End
-  //       dispatch({ type: "SET_MONTH_END" });
-  //       return;
-  //     case 36: //Home
-  //       dispatch({ type: "SET_MONTH_START" });
-  //       return;
-  //     case 37: //Left
-  //       dispatch({ type: "SET_PREVIOUS_DAY" });
-  //       return;
-  //     case 38: //Up
-  //       dispatch({ type: "SET_PREVIOUS_WEEK" });
-  //       return;
-  //     case 39: //Right
-  //       dispatch({ type: "SET_NEXT_DAY" });
-  //       return;
-  //     case 40: //Down
-  //       dispatch({ type: "SET_NEXT_WEEK" });
-  //       return;
-  //     default:
-  //       return;
-  //   }
-  // }
-
+  console.log(state === prevState);
+  
   return (
     <div>
       <div className="calendar">
@@ -69,7 +72,7 @@ const App = () => {
           role="grid"
           tabIndex="0"
           aria-label="Month"
-          onKeyDown={handleTableKeyPress(e => e, state, dispatch)}
+          onKeyDown={e => setE(e)}
         >
           <thead>{days}</thead>
           <tbody>
